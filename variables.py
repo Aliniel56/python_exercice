698ac0d1c45c48110361dbfdb2814eefd75f9872
Jeremie Le Dortz <jeremield@laposte.net>
2026-05-20 21:25:31 +0200

    Ajoute deuxieme exercice

---
+++ variables.py
@@ -0,0 +1,11 @@
+prénom = "Jerem"
+age = 25
+taille = 1.85
+aime_le_cafe = True
+
+print(f"Bonjour, je m'appelle {prénom}, j'ai {age} ans, je mesure {taille} et j'aime le cafe : {aime_le_cafe}")
+
+print(type(prénom))
+print(type(age))
+print(type(taille))
+print(type(aime_le_cafe))
