2286c40d1f9868c30f8772a8edb0db64872af7cd
Jeremie Le Dortz <jeremield@laposte.net>
2026-05-20 21:31:50 +0200

    Ajoute reponse exercice 6

---
+++ exercice_6.py
@@ -0,0 +1,4 @@
+print("Pour moi ce qui ne va pas dans le code de l'exemple c'est que le input nous sort une variable str et non int. La correction devrai être :")
+age = int(input("Ton age : "))
+age_dans_10_ans = age + 10
+print(age_dans_10_ans)
