73fa13e9f2d37c72c97d83017912ce3ebd97c3f4
Jeremie Le Dortz <jeremield@laposte.net>
2026-05-20 21:26:29 +0200

    Ajoute cinquieme exercice

---
+++ temperature.py
@@ -0,0 +1,5 @@
+temperature = int(input("Quel temperature en degrés Celsius fait il en ce moment ? (répond en chiffre uniquement)"))
+temperature_Fahrenheit = temperature * 9/5 + 32
+
+print(f"Il fait donc {temperature_Fahrenheit} degrés Fahrenheit !")
+
