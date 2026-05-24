358ed6ab753de2a557ffb3708c8f10ebda00d21f
Jeremie Le Dortz <jeremield@laposte.net>
2026-05-24 16:43:35 +0200

    Corrige l'accord d'accès

--- chapitre-2/tests_autonome.py
+++ chapitre-2/tests_autonome.py
@@ -1,17 +1,13 @@
-age = int(input("Quel est ton age ?"))
-is_member = input("Es-tu membre ?")
+age = int(input("Quel est ton age ? "))
+is_member_text = input("Es-tu membre ? ")
+is_member = is_member_text.lower().strip() == "oui"
+
 print(f"Tu as donc {age} ans")
+
 if age >= 18:
     print("Tu es majeur !")
 else:
     print("Tu es mineur !")
-
-if is_member == "oui":
-    is_member = True
-elif is_member == "Oui":
-    is_member = True
-else:
-    is_member = False
 
 if age >= 18 and is_member:
     print("Accès autorisé")
