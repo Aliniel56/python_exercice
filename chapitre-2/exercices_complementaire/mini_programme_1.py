799e2e35a112bdb2ad69bceca882a9e03f36477c
Jeremie Le Dortz <jeremield@laposte.net>
2026-05-24 22:48:22 +0200

    Ajoute le premier mini programme

---
+++ chapitre-2/exercices_complementaire/mini_programme_1.py
@@ -0,0 +1,20 @@
+how_many_notes = int(input("Combien de notes veux tu rentrer ? : "))
+
+first_note = float(input("Qu'elle est la note à rentrer ? : "))
+
+total_notes = 0
+highest = first_note
+lowest = first_note
+
+for i in range(how_many_notes - 1):
+    notes = float(input("Qu'elle est la note à rentrer ? : "))
+    total_notes += notes
+    if notes > highest:
+        highest = notes
+    if notes < lowest:
+        lowest = notes
+moyenne = total_notes / how_many_notes
+print(f"La moyenne est de : {moyenne} points !")
+print(f"Meilleure note : {highest}")
+print(f"Moins bonne note : {lowest}")
+
