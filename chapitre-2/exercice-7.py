f1bc9f1d55151fe12d14a0d5907139c974b12414
Jeremie Le Dortz <jeremield@laposte.net>
2026-05-24 20:00:19 +0200

    Ajoute l'exercice 7 du chapitre 2 sur PYTHON

---
+++ chapitre-2/exercice-7.py
@@ -0,0 +1,8 @@
+#Exercice 7
+password = "python123"
+password_input = input("Entre ton mot de passe : ")
+
+while password_input != password:
+    password_input = input("Entre ton mot de passe : ")
+print("Accès autorisé")
+
