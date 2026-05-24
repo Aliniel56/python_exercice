fa84135129c4cfd4c010b8cb07ed96a570171781
Jeremie Le Dortz <jeremield@laposte.net>
2026-05-24 19:11:46 +0200

    Ajoute l'exercice 5 du chapitre 2 sur PYTHON

---
+++ chapitre-2/exercice-5.py
@@ -0,0 +1,5 @@
+start_number = int(input("Choisir un chiffre de départ pour le compte à rebours : "))
+
+while start_number >= 0:
+    print(f"Compteur : {start_number}")
+    start_number = start_number -1
