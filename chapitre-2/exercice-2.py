ac0d2b61cd04ee6776f9bbdf6a722a84bb5e824e
Jeremie Le Dortz <jeremield@laposte.net>
2026-05-24 17:16:52 +0200

    Ajoute l'exercice 2 du chapitre 2 sur PYTHON

---
+++ chapitre-2/exercice-2.py
@@ -0,0 +1,11 @@
+note = int(input("Quel note /20 as tu eu à l'exercice ? "))
+if note >= 16:
+    print("Mention Très Bien !")
+elif note >= 14:
+    print("Mention Bien !")
+elif note >= 12:
+    print("Mention Assez Bien !")
+elif note >= 10:
+    print("Reçu sans mention !")
+else:
+    print("Recalé !")
