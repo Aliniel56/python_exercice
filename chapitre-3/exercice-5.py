5dfa60bff7d5850b31bfb4b9e3f8d2b146f2daab
Jeremie Le Dortz <jeremield@laposte.net>
2026-05-25 16:59:40 +0200

    Ajoute l'exercice 5 du chapitre 3 sur PYTHON

---
+++ chapitre-3/exercice-5.py
@@ -0,0 +1,14 @@
+def is_even(number):
+    if number % 2 ==0:
+        return True
+    else:
+        return False
+    
+print(is_even(5))
+
+def is_even_2(number):
+    return number % 2 == 0
+
+print(is_even_2(5))
+
+#Les deux renvoie un résultat identique, mais je ne sais pas pourquoi, sûrement une histoire sur le fais qu'un résultat ne peut être que True ou False, mais je n'arriverai pas à l'expliquer clairement.
