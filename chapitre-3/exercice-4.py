d5ee2a4cc91497ef4f98ab027eebf8479805569a
Jeremie Le Dortz <jeremield@laposte.net>
2026-05-25 16:52:11 +0200

    Ajoute l'exercice 4 du chapitre 3 sur PYTHON

---
+++ chapitre-3/exercice-4.py
@@ -0,0 +1,7 @@
+def rectangle_area(width, height):
+    return width * height
+def rectangle_perimeter(width, height):
+    return ((width*2) + (height * 2))
+
+print(rectangle_area(20,30))
+print(rectangle_perimeter(20,30))
