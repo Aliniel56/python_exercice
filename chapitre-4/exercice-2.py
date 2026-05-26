5ea2853a957108c9b529b642c3ce01fad1b49854
Jeremie Le Dortz <jeremield@laposte.net>
2026-05-26 19:20:54 +0200

    Ajoute l'exercice 2 du chapitre 4 sur PYTHON

---
+++ chapitre-4/exercice-2.py
@@ -0,0 +1,8 @@
+#Exercice 2
+fruits = ["Banane", "Fraise", "Kiwi"]
+fruits.append("Pomme")
+fruits.insert(0, "Framboise")
+fruits.remove("Banane")
+
+print(fruits)
+print(f"Il y a {len(fruits)} fruits dans ma liste de course")
